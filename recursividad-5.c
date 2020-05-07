14b85b43c0d9cd7b5e6023cc52f670c244e719ff
SadenPain <saschapaezsk8@gmail.com>
2020-05-07 21:52:57 +0200

    ejercicios

---
+++ recursividad-5.c
@@ -0,0 +1,86 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  recursividad-5.c
+ *
+ *    Description:  Dado un vector ordenado crecientemente A[1..n], n>=1, calcula
+ *    la forma recursiva la longitud de la escalera mas larga, es decir, la longitud
+ *    de la secuencia mas larga de valores consecutivos que se encuentre en A.
+ *
+ *        Version:  1.0
+ *        Created:  05/03/2020 10:52:15 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author: Sascha Paez
+ *   Organization: Universidad de Cadiz
+ *
+ * =====================================================================================
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  r_func (int *v, int size_v, int counter, int max_counter)
+ *  Description:  Calculate and returns the maximum long scale in array
+ * =====================================================================================
+ */
+int r_func (int *v, int size_v, int counter, int max_counter, int i) { 
+  
+  if (counter > max_counter)
+    max_counter = counter;
+
+  if ( i == size_v ) 
+    return max_counter;
+
+  if (v[i] == v[i-1] + 1) 
+    return r_func ( v, size_v, counter+1, max_counter, i+1);
+  else 
+    return r_func ( v, size_v, 1, max_counter, i+1);
+}		/* -----  end of function r_func ----- */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  func_call
+ *  Description:  Call the final recursive function
+ * =====================================================================================
+ */
+int func_call (int *v, int size_v) {
+  
+  int i = 1, counter = 1, max_counter = 0; 
+
+  if ( size_v == 0)
+    return 0;
+  if ( size_v == 1 )
+    return v[0];
+  else 
+    return r_func ( v, size_v, counter, max_counter, i );
+}		/* -----  end of function func_call  ----- */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  Main function of the program
+ * =====================================================================================
+ */
+int main (int argc, char *argv[]) {
+  int *v, size_v, longi;
+
+  printf("Tamanio del vector: ");
+  scanf("%d", &size_v);
+
+  for ( int i = 0; i < size_v; i++) {
+    printf("Number(%d): ", i);
+    scanf("%d", &v[i]);
+  }
+  
+  longi = func_call(v, size_v);
+  printf("%d", longi); 
+
+  return 0;
+}
---
+++ recursividad-6.c
@@ -0,0 +1,85 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  recursividad-6.c
+ *
+ *    Description:  Algoritmo recursivo que determine si en un vector A de n enteros
+ *    existen dos parejas consecutivas de elementos tales que las sumas sean identicas.
+ *
+ *        Version:  1.0
+ *        Created:  05/04/2020 02:48:05 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sascha Paez 
+ *   Organization:  Universidad de Cadiz
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  r_func
+ *  Description:  Si la suma de una pareja es igual a la pareja consecutiva
+ *  devuelve 1, si no devuelve 0.
+ * =====================================================================================
+ */
+int r_func ( int *v, int A_size, int i, int k, int suma )
+{
+  if ( i == A_size )
+    return 0;
+  if ( k < 2 )
+  {
+    suma += v[i];
+    return r_func ( v, A_size, i+1, k+1, suma ); 
+  }
+  if ( k == 2 )
+    if (suma == v[i] + v[i+1])
+      return 1;
+    else
+      return r_func ( v, A_size, i, 0, 0 ); 
+}		/* -----  end of function r_func  ----- */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  func_call
+ *  Description:  Llama a la funcion recursiva
+ * =====================================================================================
+ */
+int func_call ( int *v, int A_size ) 
+{
+  int suma = 0, i = 0, k = 0;
+  if ( A_size <= 2 )
+    return -1;
+  else 
+    return r_func ( v, A_size, i ,k ,suma); 
+
+}		/* -----  end of function func_call  ----- */
+
+int main ( int argc, char *argv[] ) {
+  
+  int *A, A_size, partner;
+  
+  printf ( "Size of vector: ");
+  scanf( "%d", &A_size );
+  
+  for ( int i = 0; i < A_size; i++ ) {
+    printf( "Number(%d): ", i );
+    scanf( "%d", &A[i] );
+  }
+  
+  partner = func_call ( A, A_size );
+  printf( "%d\n", partner );
+  return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
+
+
+
+
+
